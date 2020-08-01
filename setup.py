7610e7722320db176b77c4ef0535b78c9bf37ec8
Akashtyagi08 <akashtyagi@qainfotech.com>
2020-08-01 13:49:04 +0530

    Initial release

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Hencoder",
+    version="0.0.4",
+    author="Akash Tyagi",
+    author_email="akashtyagi.ta@gmail.com",
+    description="An text encoder based on Hauffman Encoding.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
